5e13f495ef1705d43952e0c63ad5ae49f49ff433
Evan Kelch <evan.c.kelch@gmail.com>
2023-04-29 16:01:00 -0400

    Added enter button with CLC/Enter functionality on calculator, overflowx-scroll on navbar for mobile.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,14 @@
 <div class="mb-2 sticky top-0 bg-zinc-800/50 backdrop-blur-sm transition-transform ease-in"
      class:-translate-y-full={offscreen}
      bind:clientHeight={headerHeight}>
-    <div class="mx-auto justify-items-center">
-        <nav class="flex flex-row py-3 justify-center space-x-8">
+    <div class="justify-items-center">
+        <nav class="flex flex-row py-3 px-3 md:justify-center space-x-5 overflow-x-auto">
             <a href="/" class="text-zinc-200 opacity-80 hover:opacity-100">Home</a>
             <a href="/blog" class="opacity-80 hover:opacity-100">Blog</a>
             <a href="/development" class="opacity-80 hover:opacity-100">Development</a>
-            <a href="/photography" class="opacity-80 hover:opacity-100">Photography</a>
-<!--            <a href="/about" class="opacity-80 hover:opacity-100">About</a>-->
             <a href="/apps/calc" class="opacity-80 hover:opacity-100">Calculator</a>
+            <a href="/photography" class="opacity-80 hover:opacity-100">Photography</a>
+            <a href="/about" class="opacity-80 hover:opacity-100">About</a>
         </nav>
     </div>
     <div class="h-px m-0 bg-gradient-to-r from-indigo-500 to-red-500"></div>
--- src/routes/apps/calc/+page.svelte
+++ src/routes/apps/calc/+page.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-    import {all, create, addDependencies, subtractDependencies, divideDependencies, multiplyDependencies, parserDependencies} from "mathjs";
+    import {create, addDependencies, subtractDependencies, divideDependencies, multiplyDependencies, parserDependencies} from "mathjs";
 
-    const {add, subtract, divide, format, parser} = create({addDependencies, subtractDependencies, divideDependencies, multiplyDependencies, parserDependencies});
+    const {format, parser} = create({addDependencies, subtractDependencies, divideDependencies, multiplyDependencies, parserDependencies});
 
     let allowedPush = ['1','2','3','4','5','6','7','8','9','0', '-', '+', '*', '/', 'e', '.']
     let allowedSpecial = ['c','Enter', 'Backspace']
     let expression = '';
     let result = '';
 
-    function expPush(add: string) {
+    function pushExp(add: string) {
         expression = expression.concat(add)
     }
 
@@ -18,7 +18,7 @@
         if (val === '/')
             key.preventDefault()
         if (allowedPush.includes(val))
-            expPush(val)
+            pushExp(val)
         else if (allowedSpecial.includes(val)) {
             if (val==='c')
                 expression = '';
@@ -44,35 +44,40 @@
         <p>{expression}</p>
     </div>
 
-    <div class="h-24 p-1 bg-zinc-800 text-white border-2 border-cyan-600/80 my-2 rounded-sm">
-        <p>{result}</p>
+    <div class="grid grid-cols-5 space-x-1">
+        <div class="col-span-4 h-24 p-1 bg-zinc-800 text-white border-2 border-cyan-600/80 my-2 rounded-sm">
+            <p>{result}</p>
+        </div>
+        <div>
+            <button on:click={()=>handleKeypress({key: 'Enter'})} class="w-full text-black bg-amber-500 h-24 p-1 my-2 rounded-sm border-2 border-red-500/50">Enter</button>
+        </div>
     </div>
 
     <div class="bg-zinc-800 text-white my-2 rounded-md">
         <div class="grid grid-cols-5 gap-1">
-            <button on:click={()=>expPush('e')} class="calc-op">EE</button>
-            <button on:click={()=>expPush('1')} class="calc-num">1</button>
-            <button on:click={()=>expPush('2')} class="calc-num">2</button>
-            <button on:click={()=>expPush('3')} class="calc-num">3</button>
-            <button on:click={()=>expPush('-')} class="calc-op">-</button>
+            <button on:click={()=>pushExp('e')} class="calc-op">EE</button>
+            <button on:click={()=>pushExp('1')} class="calc-num">1</button>
+            <button on:click={()=>pushExp('2')} class="calc-num">2</button>
+            <button on:click={()=>pushExp('3')} class="calc-num">3</button>
+            <button on:click={()=>pushExp('-')} class="calc-op">-</button>
 
             <button class="calc-op">&radic;</button>
-            <button on:click={()=>expPush('4')} class="calc-num">4</button>
-            <button on:click={()=>expPush('5')} class="calc-num">5</button>
-            <button on:click={()=>expPush('6')} class="calc-num">6</button>
-            <button on:click={()=>expPush('+')} class="calc-op">+</button>
+            <button on:click={()=>pushExp('4')} class="calc-num">4</button>
+            <button on:click={()=>pushExp('5')} class="calc-num">5</button>
+            <button on:click={()=>pushExp('6')} class="calc-num">6</button>
+            <button on:click={()=>pushExp('+')} class="calc-op">+</button>
 
             <button class="calc-spec">^</button>
-            <button on:click={()=>expPush('7')} class="calc-num">7</button>
-            <button on:click={()=>expPush('8')} class="calc-num">8</button>
-            <button on:click={()=>expPush('9')} class="calc-num">9</button>
-            <button on:click={()=>expPush('*')} class="calc-op">*</button>
+            <button on:click={()=>pushExp('7')} class="calc-num">7</button>
+            <button on:click={()=>pushExp('8')} class="calc-num">8</button>
+            <button on:click={()=>pushExp('9')} class="calc-num">9</button>
+            <button on:click={()=>pushExp('*')} class="calc-op">*</button>
 
-            <button on:click={()=>expPush('c')} class="calc-spec">CLC</button>
+            <button on:click={()=>handleKeypress({key:'c'})} class="calc-spec">CLC</button>
             <button class="calc-spec">(&#60;->)</button>
-            <button on:click={()=>expPush('0')} class="calc-num">0</button>
+            <button on:click={()=>pushExp('0')} class="calc-num">0</button>
             <button class="calc-spec">->()&#60;-</button>
-            <button on:click={()=>expPush('/')} class="calc-op">/</button>
+            <button on:click={()=>pushExp('/')} class="calc-op">/</button>
         </div>
     </div>
 
