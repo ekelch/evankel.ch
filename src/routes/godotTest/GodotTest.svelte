707bd6bd1a4d61e63889076a79decb59f8dcb51f
Evan <evan.c.kelch@gmail.com>
2025-03-05 21:46:57 -0500

    game opening in new tab for now

--- src/routes/godotTest/GodotTest.svelte
+++ src/routes/godotTest/GodotTest.svelte
@@ -1,14 +1,31 @@
 <script lang="ts">
-    import gameIndex from "$lib/assets/gameFiles/index.html"
+    import gameImg from "/src/lib/assets/godotGame.png"
+
+    const gameSrc = "https://itch.io/embed-upload/12999334?color=333333&uuid=c35a06c029c20fb1a0ec9a21d96776ba"
+    const openGame = () => {
+        window.open(gameSrc, "_blank")
+    }
 </script>
 
-<p>
-    embedded godot game coming soon! (pressing R won't do anything yet)
-</p>
-<iframe src={gameIndex} height="100%" width="100%"/>
+<div id="game-container">
+    <p>I would like to add an embedded version but for now it will open in new tab.</p>
+    <button on:click={openGame}>
+        <img src={gameImg} alt="game placeholder" />
+    </button>
+</div>
 
 <style>
+    #game-container {
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+    }
+    button {
+        all: unset;
+    }
     img {
-        height: 600px;
+        max-width: 80%;
+        object-fit: contain;
+        cursor: pointer;
     }
 </style>
